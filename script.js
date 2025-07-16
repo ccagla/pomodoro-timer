4d7a4760642bd14c9355f19acd36b0d4b1b8cc77
cagla eksi <caglaeksii@gmail.com>
2025-07-16 10:45:15 +0300

    feat: start and update pomodoro countdown timer

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const timeDisplay = document.getElementById("time");
+const startBtn = document.getElementById("start");
+const pauseBtn = document.getElementById("pause");
+const resetBtn = document.getElementById("reset");
+
+let time = 25*60;
+let interval;
+
+function startTimer() {
+    if (interval) return;
+
+    interval = setInterval(() => {
+        if (time <= 0) {
+            clearInterval(interval);
+            interval = null;
+            return;
+        }
+        time--;
+        updateTime();
+    }, 1000);
+}
+
+//startTimer();
+
+function updateTime() {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    const formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+
+    timeDisplay.textContent = `${minutes}:${formattedSeconds}`
+}
+
